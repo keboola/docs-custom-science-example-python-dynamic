72dfdd336b9ee479dc6f58a6bf8097cf3705a9ba
Odin <odinuv@gmail.com>
2016-03-21 15:35:56 +0100

    chore: pep8 names

--- main.py
+++ main.py
@@ -8,29 +8,29 @@ cfg = docker.Config('/data/')
 tables = cfg.getInputTables()
 j = 0
 for table in tables:
-    # get csv file name 
-    inName = table['destination'] 
-    
+    # get csv file name
+    inName = table['destination']
+
     # read input table metadata
-    manifest = cfg.getTableManifest(inName)
+    manifest = cfg.get_table_manifest(inName)
 
     # get csv file name with full path from output mcfging
-    outName = cfg.getExpectedOutputTables()[j]['full_path']
+    outName = cfg.get_expected_output_tables()[j]['full_path']
 
     # get file name from output mcfging
-    outDestination = cfg.getExpectedOutputTables()[j]['destination']
+    outDestination = cfg.get_expected_output_tables()[j]['destination']
 
     # get csv full path and read table data
     i = 0
-    with open(table['full_path'], mode='rt', encoding='utf-8') as inFile, open(outName, mode='wt', encoding='utf-8') as outFile:
+    with open(table['full_path'], mode='rt', encoding='utf-8') as in_file, open(outName, mode='wt', encoding='utf-8') as out_file:
         # read input file line-by-line
-        lazyLines = (line.replace('\0', '') for line in inFile)
-        csvReader = csv.DictReader(lazyLines, dialect='kbc')
+        lazy_lines = (line.replace('\0', '') for line in in_file)
+        csvReader = csv.DictReader(lazy_lines, dialect='kbc')
         headers = csvReader.fieldnames
         headers.extend(['primaryKey'])
-        
+
         # write output file header
-        writer = csv.DictWriter(outFile, fieldnames = headers, dialect='kbc')
+        writer = csv.DictWriter(out_file, fieldnames=headers, dialect='kbc')
         writer.writeheader()
 
         for row in csvReader:
@@ -39,15 +39,15 @@ for table in tables:
                 i = i + 1
                 row['primaryKey'] = i
             else:
-                row['primaryKey'] = NULL
-   
+                row['primaryKey'] = None
+
             writer.writerow(row)
-   
+
     if (len(manifest['primary_key']) == 0):
         pk = ['primaryKey']
     else:
         pk = manifest['primary_key']
 
     # write table metadata - set new primary key
-    cfg.writeTableManifest(outName, destination = outDestination, primaryKey = pk)
+    cfg.writeTableManifest(outName, destination=outDestination, primary_key=pk)
     j = j + 1
